66f25278e1d5f5cb12b16edae14618ecc64ebb37
MarlonDeOcampo <jezozobrado@gmail.com>
2023-08-07 15:03:20 +0800

    Implement topbar.

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,7 +1,34 @@
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Topbar = () => {
-  return <div>Topbar</div>;
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden ">Yarn</p>
+      </Link>
+      <div className="flex items-center gap-1">
+        <div className="block ">
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer md:hidden">
+                <Image
+                  src="/assets/logout.svg"
+                  alt="logout"
+                  width={24}
+                  height={24}
+                ></Image>
+              </div>
+            </SignOutButton>
+          </SignedIn>
+        </div>
+        <OrganizationSwitcher />
+      </div>
+    </nav>
+  );
 };
 
 export default Topbar;
