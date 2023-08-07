66d5b29d969c38cac8e1e9e895ce7976bee4953b
MarlonDeOcampo <jezozobrado@gmail.com>
2023-08-07 16:21:09 +0800

    Implement rightsidebar.

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { dark } from "@clerk/themes";
 import React from "react";
 
 const Topbar = () => {
@@ -25,7 +26,14 @@ const Topbar = () => {
             </SignOutButton>
           </SignedIn>
         </div>
-        <OrganizationSwitcher />
+        <OrganizationSwitcher
+          appearance={{
+            baseTheme: dark,
+            elements: {
+              organizationSwitcherTrigger: "py-2 px-4",
+            },
+          }}
+        />
       </div>
     </nav>
   );
