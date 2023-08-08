b9f4a840508066df1ce275eb66f45423c13ae6ce
MarlonDeOcampo <jezozobrado@gmail.com>
2023-08-08 10:59:09 +0800

    Implement fetchUser BE

--- app/(root)/spin-yarn/page.tsx
+++ app/(root)/spin-yarn/page.tsx
@@ -1,7 +1,23 @@
+import SpinYarn from "@/components/forms/SpinYarn";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const page = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect("/onboarding");
+  return (
+    <>
+      <h1 className="head-text">Spin a yarn</h1>
+      <SpinYarn userId={userInfo._id} />
+    </>
+  );
 };
 
 export default page;
---
+++ components/forms/SpinYarn.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SpinYarn = ({ userId }: { userId: string }) => {
+  return <div>SpinYarn</div>;
+};
+
+export default SpinYarn;
